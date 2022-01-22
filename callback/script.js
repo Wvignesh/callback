fbc4a542f4f906d7871f2136ddffe83887924e71
vignesh <96234695+Wvignesh@users.noreply.github.com>
2022-01-22 16:16:57 +0530

    hell

---
+++ callback/script.js
@@ -0,0 +1,67 @@
+var a=11;
+ 
+  setTimeout(function(){
+
+    a--;
+    
+    document.getElementById('second').innerHTML=a;
+
+    setTimeout(function(){
+      a--;
+      document.getElementById('second').innerHTML=a;
+
+      setTimeout(function(){
+
+        a--;
+        document.getElementById('second').innerHTML=a;
+        setTimeout(function(){
+          a--;
+        document.getElementById('second').innerHTML=a;
+        setTimeout(function(){
+          a--;
+        document.getElementById('second').innerHTML=a;
+
+        setTimeout(function(){
+          a--;
+        document.getElementById('second').innerHTML=a;
+
+        setTimeout(function(){
+          a--;
+        document.getElementById('second').innerHTML=a;
+
+        setTimeout(function(){
+          a--;
+        document.getElementById('second').innerHTML=a;
+
+        setTimeout(function(){
+          a--;
+        document.getElementById('second').innerHTML=a;
+
+        setTimeout(function(){
+          a--;
+        document.getElementById('second').innerHTML=a;
+  
+        setTimeout(function(){
+
+          document.getElementById('second').innerHTML=" Happy Independence Day"
+        },1000)
+ 
+
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+        },1000)
+
+        },1000)
+
+        },1000)
+        },1000)
+
+        },1000)
+
+      },1000)
+    
+      
+    
+ 
